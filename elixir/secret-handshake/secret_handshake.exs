711ad16df0fe9bb1125822f9b4ac01090572b8cb
ArtemGordinsky <artem@gordinskiy.com>
2018-11-17 20:29:21 +0100

    Solve the "Secret Handshake" in Elixir

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -1,4 +1,15 @@
 defmodule SecretHandshake do
+  use Bitwise
+
+  @action_codes %{
+    0b1 => "wink",
+    0b10 => "double blink",
+    0b100 => "close your eyes",
+    0b1000 => "jump"
+  }
+
+  @reverse_code 0b10000
+
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -15,6 +26,22 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    Enum.reduce(@action_codes, [], fn {bin_code, action}, commands ->
+      if (code &&& bin_code) > 0 do
+        commands ++ [action]
+      else
+        commands
+      end
+    end)
+    |> maybe_reverse(code)
+  end
+
+  @spec maybe_reverse(commands :: list, code :: binary) :: list(String.t())
+  defp maybe_reverse(commands, code) when (code &&& @reverse_code) > 0 do
+    Enum.reverse(commands)
+  end
+
+  defp maybe_reverse(commands, _code) do
+    commands
   end
 end
-
